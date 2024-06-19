443269c72442af93495220f47d446e5da2982bd1
edgeconnect-codecommit-user01-at-381492305313 <edgeconnect-codecommit-user01-at-381492305313>
2024-06-19 12:14:24 +0100

    new_config - 1st commit

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'eu-west-2_XGGgZXiNc', // e.g. eu-west-2_XGGgZXiNc
+        userPoolClientId: '5uj09s3ceodmibluvhep7inq6p', // e.g. 5uj09s3ceodmibluvhep7inq6p
+        region: 'eu-west-2'
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://rc7nyt4tql.execute-api.eu-west-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.eu-west-2.amazonaws.com/prod',
     }
 };
